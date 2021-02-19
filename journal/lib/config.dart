adab25c1a5e154f3040705cee9285f911de4add4
Pat Stumbaugh <stumbaup@oregonstate.edu>
2021-02-19 09:33:05 -0800

    added theme change button

---
+++ journal/lib/config.dart
@@ -0,0 +1,5 @@
+library config.globals;
+
+import 'main.dart';
+
+MyTheme currentTheme = MyTheme();
--- journal/lib/main.dart
+++ journal/lib/main.dart
@@ -1,22 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:journal/widgets/journal_entry_form.dart';
 import './screens/journal_entries.dart';
+import 'config.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      print('Changing theme');
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        themeMode: currentTheme.currentTheme(),
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
         home: Scaffold(
-            appBar: AppBar(title: Text('Journal')),
+            appBar: AppBar(
+              title: Text('Journal'),
+              actions: <Widget>[
+                PopupMenuButton<String>(
+                  onSelected: handleClick,
+                  itemBuilder: (BuildContext context) {
+                    return {'Dark Mode'}.map((String choice) {
+                      return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+                  },
+                ),
+              ],
+            ),
             body: JournalEntriesScreen()));
+  }
+}
+
+void handleClick(String value) {
+  switch (value) {
+    case 'Dark Mode':
+      currentTheme.switchTheme();
+  }
+}
+
+class MyTheme with ChangeNotifier {
+  static bool _isDark = true;
+
+  ThemeMode currentTheme() {
+    return _isDark ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  void switchTheme() {
+    _isDark = !_isDark;
+    notifyListeners();
   }
 }
