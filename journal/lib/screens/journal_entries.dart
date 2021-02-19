d1c4272def877975776055ae9eaba79ea345ee37
Pat Stumbaugh <stumbaup@oregonstate.edu>
2021-02-18 21:35:58 -0800

    test print(index)

--- journal/lib/screens/journal_entries.dart
+++ journal/lib/screens/journal_entries.dart
@@ -9,7 +9,7 @@ class JournalEntriesScreen extends StatefulWidget {
 }
 
 class _JournalEntriesScreenState extends State<JournalEntriesScreen> {
-  final items = List<Map>.generate(10000, (i) {
+  final items = List<Map>.generate(1000, (i) {
     return {
       'title': 'Journal Entry $i',
       'subtitle': 'Subtitle text for $i',
@@ -18,7 +18,7 @@ class _JournalEntriesScreenState extends State<JournalEntriesScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return Scaffold(body: ListView.builder(
       //itemCount: /*number of items in the list, if known*/,
       itemBuilder: (context, index) {
         return ListTile(
@@ -29,61 +29,30 @@ class _JournalEntriesScreenState extends State<JournalEntriesScreen> {
           //onTap: () {Navigator.of(context).pushNamed(/*route name here*/)},
         );
       },
-    );
+    ));
   }
 }
+
 
 
 /*
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(title: Text('${desserts.length} Desserts')),
-            body: DessertsScreen(desserts: desserts),
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  addDessert('New Cake! ${desserts.length}');
-                },
-                child: Icon(Icons.cake))));
-  }
-
-  void addDessert(String name) {
-    // DONE
-    setState(() {
-      desserts.add(name);
-    });
-  }
-}
-
-class DessertsScreen extends StatefulWidget {
-  final List<String> desserts;
-
-  DessertsScreen({this.desserts});
-
-  @override
-  DessertsScreenState createState() => DessertsScreenState(desserts: desserts);
-}
-
-class DessertsScreenState extends State<DessertsScreen> {
-  final List<String> desserts;
-
-  DessertsScreenState({this.desserts});
-
-  @override
-  Widget build(BuildContext context) {
-    // DONE
-    return ListView.builder(
-        itemCount: desserts.length,
-        itemBuilder: (context, index) {
-          return ListTile(
+    return JournalScaffold(
+        title: 'Journal Entries',
+        child: ListView.builder(
+          //itemCount: /*number of items in the list, if known*/,
+          itemBuilder: (context, index) {
+            return ListTile(
               leading: FlutterLogo(),
-              trailing: Icon(Icons.cake),
-              title: Text(desserts[index]),
-              subtitle: Text('Mmmm delicious!'));
-        });
+              trailing: Icon(Icons.more_horiz),
+              title: Text('Journal Entry ${items[index]['title']}'),
+              subtitle: Text('Example subtitle ${items[index]['subtitle']}'),
+              onTap: () {Navigator.of(context).pushNamed(/*route name here*/)},
+            );
+          },
+        ));
   }
 }
 */
+
